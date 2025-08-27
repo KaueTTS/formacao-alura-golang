22405b76752533a93eafc7752f9b7995073e7d17
kaue.oliveira <kaue.oliveira@grupomulti.com.br>
2025-08-27 18:41:53 -0300

    first commit

--- go_desenvolvendo_uma_api_rest/controllers/controllers.go
+++ go_desenvolvendo_uma_api_rest/controllers/controllers.go
@@ -3,9 +3,9 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"go_desenvolvendo_uma_api_rest/database"
 	"go_desenvolvendo_uma_api_rest/models"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -16,26 +16,19 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 func Personalidades(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(models.Personalidades)
+
+	var p []models.Personalidade
+	database.DB.Find(&p)
+	json.NewEncoder(w).Encode(p)
 }
 
 func PersonalidadePorID(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	paramID, err := strconv.Atoi(id)
-	if err != nil {
-		http.Error(w, "ID inválido", http.StatusBadRequest)
-		return
-	}
+	var personalidade models.Personalidade
+	database.DB.First(&personalidade, id)
 
-	for _, personalidade := range models.Personalidades {
-		if personalidade.Id == paramID {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(personalidade)
-			return
-		}
-	}
-
-	http.Error(w, "Personalidade não encontrada", http.StatusNotFound)
+	json.NewEncoder(w).Encode(personalidade)
 }
